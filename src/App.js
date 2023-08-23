a2f7c100c8ebf7f9c10a36567cdd4dc0a7c1e034
Anshu Sharma <anshusharma6327@gmail.com>
2023-08-23 16:23:10 +0530

    intiail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./screens/Main.js";
+import { useEffect } from "react";
+import Aos from "aos";
+import { Toaster } from "react-hot-toast";
 
-function App() {
+const Root = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Toaster />
+      <Outlet />
+    </>
   );
-}
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Main />} />
+    </Route>
+  )
+);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div className="justify-center  md:p-10 lg:p-20 p-8 md:items-center grid grid-cols-1 gap-10 md:grid-cols-2 items-center ">
+      <div className="flex flex-col text-white relative">
+        <h1 className="md:text-6xl md:leading-[80px] leading-[60px] text-3xl tracking-wide font-semibold md:font-bold">
+          Decentralized Token
+          <br className="hidden md:inline" /> Governed By The
+          <br className="hidden md:inline" />
+          <span className="text-[#43C878]"> Community</span>
+        </h1>
+        <p className="md:text-xl mt-4 text-base">
+          Stake and win amazing rewards with multiple level
+          <br className="hidden md:inline" />
+          plan of this platform.
+        </p>
+        <div className="flex gap-4 w-full rounded-full mt-6 md:flex-row md:px-2 max-w-fit flex-col border-[#43C878] border justify-center items-center">
+          <Link
+            to="/"
+            className="p-2 w-full "
+          >
+            <img src="/assets/coinsut2.svg" alt="notfound" />
+          </Link>
+        </div>
+        <div
+          className="absolute inset-0"
+          style={{
+            content: "",
+            position: "absolute",
+            zIndex: "0",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: "50%",
+            height: "100%",
+            borderRadius: "50%",
+            background:
+              "linear-gradient(180deg, rgba(44, 158, 117, 0.80) 0%, rgba(25, 185, 137, 0.55) 100%)",
+            filter: "blur(200px)",
+          }}
+        ></div>
+      </div>
+      <div className="flex justify-center h-full flex-col items-center">
+        <img src="/assets/gerofig.gif" alt="notfound" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
