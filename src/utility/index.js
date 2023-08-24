97e7c620cf100f3c02e78d41456728ee10eb8149
Anshu Sharma <anshusharma6327@gmail.com>
2023-08-24 17:19:40 +0530

    extra package remvoed

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,9 +1,3 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-
-export const Injected = new InjectedConnector({
-  supportedChainIds: [97], //chain ids for ethereum, goerli, bsc test net , bsc main net
-});
-
 export const formatBalance = (rawBalance) => {
   const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(2);
   return balance;
