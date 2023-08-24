5ae6b6e683deae3a2bd2aa06bef52804848514ad
Anshu Sharma <71519327+anshu0x@users.noreply.github.com>
2023-08-24 17:26:04 +0530

    Update DashboardNavbar.js

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -37,7 +37,7 @@ const DashboardNavbar = ({ setOpenTab }) => {
               fill="#0FCA87"
             />
           </svg>
-          <p>Stake V4X Coin</p>
+          <p>Stake MJC Coin</p>
         </div>
       </div>
     </div>
